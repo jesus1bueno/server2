daa7e45fc46dbdd94ecaf04ad5aa5897223eda6d
PabloCarranza31 <158582000+PabloCarranza31@users.noreply.github.com>
2025-03-05 17:06:26 -0600

    creación de ruta de registro

--- Back/server/rutas/suscripcion.js
+++ Back/server/rutas/suscripcion.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { sendPush } = require('../SendPush.js');  // Usar require en lugar de import
 const subs = require('../models/subsModel.js');
+const users = require('../models/userModel.js');
 const webpush = require('web-push');
 
 router.post('/suscripcion', async (req, res) => {
@@ -19,7 +20,30 @@ router.post('/suscripcion', async (req, res) => {
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-})
+});
+
+
+// Crear un nuevo usuario 
+router.post('/registro', async (req, res) => {
+  const { email, nombre,telefono,password } = req.body;
+
+  try {
+    // Verificar si el usuario ya existe
+    const existingUser = await users.findOne({ email });
+    
+    if (existingUser) {
+      return res.status(400).json({ message: 'El correo electrónico ya está en uso' });
+    }
+
+    // Crear el nuevo usuario
+    const newUser = new users({ email, nombre,telefono,password  });
+    const savedUser = await newUser.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
 
