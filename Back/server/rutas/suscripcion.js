01d5c18f0c2b5ae73e93461a4fc621cfe30b566b
PabloCarranza31 <158582000+PabloCarranza31@users.noreply.github.com>
2025-03-05 20:31:34 -0600

    login

--- Back/server/rutas/suscripcion.js
+++ Back/server/rutas/suscripcion.js
@@ -45,5 +45,30 @@ router.post('/registro', async (req, res) => {
   }
 });
 
+// Ruta de Login
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      // Buscar usuario por email
+      const user = await users.findOne({ email });
+      
+      if (!user || user.password !== password) {
+        
+        return res.status(400).json({ message: 'Email o contraseña incorrectos' });
+      }
+  
+      // Crear y enviar token JWT
+      //const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        //expiresIn: '1h',
+      //}
+    //);
+  
+      res.json({ token , user});
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+    
+  });
+
 module.exports = router;
 
