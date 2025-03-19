0c43b2ba2399d042170108a7e657d01647500b5a
jesus1bueno <159319212+jesus1bueno@users.noreply.github.com>
2025-03-19 12:05:20 -0600

    correcion4

--- Back/server/models/userModel.js
+++ Back/server/models/userModel.js
@@ -2,10 +2,6 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  nombre: {
-    type: String,
-    required: true
-  },
   email: {
     type: String,
     required: true,
